d31230ced5a99657af7f23fcf54bedb2fe678b41
Taylor Nguyen <fyrworx@gmail.com>
2021-05-17 14:13:24 -0700

    Edited everything

--- CafeApp.java
+++ CafeApp.java
@@ -46,8 +46,7 @@ public class CafeApp {
         // Print the Breakfast or Lunch menu
         printMealType();
 
-
-        while (breakfastOrLunchSelected != true) {
+        while(breakfastOrLunchSelected != true) {
             // Input validation
             try {
                 breakfastOrLunch = scnr.nextInt();
@@ -57,7 +56,7 @@ public class CafeApp {
                     breakfastOrLunch = scnr.nextInt();
                 }
             }
-            catch (InputMismatchException e) {
+            catch(InputMismatchException e) {
                 System.out.println("Error! Number must be greater than 0.");
                 System.out.print("Select Breakfast or Lunch [1, 2]: ");
                 scnr.next();
@@ -83,15 +82,12 @@ public class CafeApp {
 
         // Declare variables
         int coffeeSelection = 0;
-        int previousCoffeeSelection = 0;
-        int coffeeCount = 0;
         boolean coffeeSelected = false;
 
-        // Print out menu
-        printCoffeeMenu();
+        while(coffeeSelected != true) {
+            // Print out menu
+            printCoffeeMenu();
 
-
-        while (coffeeSelected != true) {
             // Input validation
             try {
                 coffeeSelection = scnr.nextInt();
@@ -101,48 +97,36 @@ public class CafeApp {
                     coffeeSelection = scnr.nextInt();
                 }
             }
-            catch (InputMismatchException e) {
+            catch(InputMismatchException e) {
                 System.out.println("Error! Number must be greater than 0.");
                 System.out.print("Select a coffee [1, 5]: ");
                 scnr.next();
                 continue;
             }
-            // Assign previousCoffeeSelection to coffeeSelection's value for comparison, only for the 1st coffee selection
-            if (previousCoffeeSelection == 0) {
-                previousCoffeeSelection = coffeeSelection;
-            }
-            // This only runs when the coffee selected is the same as the previous one
-            if (coffeeSelection == previousCoffeeSelection) {
-                // Insert coffeeSelection value into coffee field within cafeOrder object
-                switch(coffeeSelection) {
-                    case 1:
-                        cafeOrder.setCoffee("Espresso");
-                        cafeOrder.addPrice(3.5);
-                        break;
-                    case 2:
-                        cafeOrder.setCoffee("Latte");
-                        cafeOrder.addPrice(3.5);
-                        break;
-                    case 3:
-                        cafeOrder.setCoffee("Cappuccino");
-                        cafeOrder.addPrice(5.0);
-                        break;
-                    case 4:
-                        cafeOrder.setCoffee("Cold Brew");
-                        cafeOrder.addPrice(3.0);
-                        break;
-                }
-                coffeeCount += 1;
-                // Reprint coffee menu for multiple orders
-                printCoffeeMenu();
-            }
-            else if (coffeeSelection == 5) {
-                coffeeSelected = true;
-            }
-            else {
-                System.out.println("You must select the same type of coffee. You have selected #" + previousCoffeeSelection);
-                System.out.print("Select a coffee [1, 5]: ");
-            }            
+
+            // Insert coffeeSelection value into coffee field within cafeOrder object
+            // This chunk of code will only execute if the input is valid
+            switch(coffeeSelection) {
+                case 1:
+                    cafeOrder.addCoffeeToOrder(1);
+                    cafeOrder.addPrice(3.5);
+                    break;
+                case 2:
+                    cafeOrder.addCoffeeToOrder(2);
+                    cafeOrder.addPrice(3.5);                   
+                    break;
+                case 3:
+                    cafeOrder.addCoffeeToOrder(3);
+                    cafeOrder.addPrice(5.0);
+                    break;
+                case 4:
+                    cafeOrder.addCoffeeToOrder(4);
+                    cafeOrder.addPrice(3.0);
+                    break;
+                case 5:
+                    coffeeSelected = true;
+                    break;
+            }          
         }
 
         ////////////////////////////////////
@@ -151,21 +135,19 @@ public class CafeApp {
 
         // Declare variables
         int foodSelection = 0;
-        int previousFoodSelection = 0;
-        int foodCount = 0;
         boolean foodSelected = false;
 
-        // Print food menu
-        printFoodMenu();
+        while(foodSelected != true) {
+            // Print out menu
+            printFoodMenu();
 
-        while (foodSelected != true) {
             // Input validation
             try {
                 foodSelection = scnr.nextInt();
                 while ((foodSelection < 1) || (foodSelection > 6)) {
                     System.out.println("Error! Number must be greater than 0.");
                     System.out.print("Select Food [1, 6]: ");
-                    coffeeSelection = scnr.nextInt();
+                    foodSelection = scnr.nextInt();
                 }
             }
             catch (InputMismatchException e) {
@@ -174,45 +156,32 @@ public class CafeApp {
                 scnr.next();
                 continue;
             }
-            // Like before, this only runs the first time. previousFoodSelection only gets assigned once
-            if (previousFoodSelection == 0) {
-                previousFoodSelection = foodSelection;
-            }
-            // This will run only if the current foodSelection matches the previous one
-            if (foodSelection == previousFoodSelection) {
-                // Insert foodSelection value into food field within cafeOrder object
-                switch(foodSelection) {
-                    case 1:
-                        cafeOrder.setFood("Tuna Sandwich");
-                        cafeOrder.addPrice(10.0);
-                        break;
-                    case 2:
-                        cafeOrder.setFood("Chicken Sandwich");
-                        cafeOrder.addPrice(10.0);
-                        break;
-                    case 3:
-                        cafeOrder.setFood("Burrito");
-                        cafeOrder.addPrice(12.0);
-                        break;
-                    case 4:
-                        cafeOrder.setFood("Yogurt Bowl");
-                        cafeOrder.addPrice(8.0);
-                        break;
-                    case 5:
-                        cafeOrder.setFood("Avocado Toast");
-                        cafeOrder.addPrice(8.0);
-                        break;
-                }
-                foodCount += 1;
-                // Reprint food menu for multiple orders
-                printFoodMenu();
-            }
-            else if (foodSelection == 6) {
-                foodSelected = true;
-            }
-            else {
-                System.out.println("You must select the same type of meal. You have selected #" + previousFoodSelection);
-                System.out.print("Select Food [1, 6]: ");
+
+            // Insert foodSelection value into food field within cafeOrder object
+            switch(foodSelection) {
+                case 1:
+                    cafeOrder.addFoodToOrder(1);
+                    cafeOrder.addPrice(10.0);
+                    break;
+                case 2:
+                    cafeOrder.addFoodToOrder(2);
+                    cafeOrder.addPrice(10.0);
+                    break;
+                case 3:
+                    cafeOrder.addFoodToOrder(3);
+                    cafeOrder.addPrice(12.0);
+                    break;
+                case 4:
+                    cafeOrder.addFoodToOrder(4);
+                    cafeOrder.addPrice(8.0);
+                    break;
+                case 5:
+                    cafeOrder.addFoodToOrder(5);
+                    cafeOrder.addPrice(8.0);
+                    break;
+                case 6:
+                    foodSelected = true;
+                    break;
             }
         }
         
@@ -230,7 +199,7 @@ public class CafeApp {
         // Skip previous buffer thingy
         customerName = scnr.nextLine();
 
-        while (customerNameSubmitted != true) {
+        while(customerNameSubmitted != true) {
             // Input validation
             try {
                 customerName = scnr.nextLine();
@@ -245,10 +214,10 @@ public class CafeApp {
         }
         
         // Assign coffeeCount and foodCount into cafeOrder
-        cafeOrder.setCoffeeCount(coffeeCount);
-        cafeOrder.setFoodCount(foodCount);
 
         // Print All
+        cafeOrder.coffeeStringOutput();
+        cafeOrder.foodStringOutput();
         cafeOrder.printAll();
 
         // Write to output
